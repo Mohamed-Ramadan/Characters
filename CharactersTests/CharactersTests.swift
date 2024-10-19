cb03a6d52756faadaedef421f029d96e1ed1a27d
Mohamed Ramadan <mohamed_r_2015@yahoo.com>
2024-10-20 00:44:10 +0300

    Initial Commit

---
+++ CharactersTests/CharactersTests.swift
@@ -0,0 +1,17 @@
+//
+//  CharactersTests.swift
+//  CharactersTests
+//
+//  Created by Mohamed Ramadan on 20/10/2024.
+//
+
+import Testing
+@testable import Characters
+
+struct CharactersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
